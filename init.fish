710dc802eeffb2e491cbe5b53698e2d4037ece02
Viet Anh Duong <vietanhs0817@gmail.com>
2025-03-20 09:31:36 +0700

    Update config
    Signed-off-by: Viet Anh Duong <vietanhs0817@gmail.com>

--- init.fish
+++ init.fish
@@ -82,6 +82,9 @@ else if test -d /opt/homebrew # MacOS
 	set -gx HOMEBREW_CASKROOM "$HOMEBREW_PREFIX/Caskroom"
 end
 fish_add_path -gP "$HOMEBREW_PREFIX/bin" "$HOMEBREW_PREFIX/sbin";
+fish_add_path -gP "$HOMEBREW_PREFIX/share/google-cloud-sdk/bin";
+
+
 ! set -q MANPATH; and set MANPATH ''; set -gx MANPATH "$HOMEBREW_PREFIX/share/man" $MANPATH;
 ! set -q INFOPATH; and set INFOPATH ''; set -gx INFOPATH "$HOMEBREW_PREFIX/share/info" $INFOPATH;
 
