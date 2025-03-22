4ec14a6b13b8dc79fe4433f3a87fb55c531b9b23
Viet Anh Duong <vietanhs0817@gmail.com>
2025-03-22 20:58:37 +0700

    Cleanup repo
    Signed-off-by: Viet Anh Duong <vietanhs0817@gmail.com>

--- reset-vm.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-pattern=$1
-pattern=${pattern:-"remote-*"}
-
-gcloud compute instances list --filter="name:${pattern}" --project=test-va-310803 --format="table[no-heading](name, zone)" | awk '{print $1 " --zone=" $2}' | xargs -n2 gcloud compute instances reset --project=test-va-310803 
