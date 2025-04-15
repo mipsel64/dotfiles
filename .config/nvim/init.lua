711adc4cc7efe0ba8228edbb4f8f3e6fcc0b7ec5
Viet Anh Duong <vietanhs0817@gmail.com>
2025-04-15 14:43:41 +0700

    Update config
    Signed-off-by: Viet Anh Duong <vietanhs0817@gmail.com>

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -76,6 +76,13 @@ vim.opt.clipboard = ""
 -- Use Ctrl+c to copy system clipboard
 vim.keymap.set("v", "<C-c>", "\"+y<CR>", { desc = "Copy selected text to system clipboard" })
 
+-- Allow clipboard copy paste in neovim
+vim.g.neovide_input_use_logo = 1
+vim.api.nvim_set_keymap('', '<D-v>', '+p<CR>', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('!', '<D-v>', '<C-R>+', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('t', '<D-v>', '<C-R>+', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('v', '<D-v>', '<C-R>+', { noremap = true, silent = true})
+
 -- highlight cursorline
 vim.opt.cursorline = true
 
