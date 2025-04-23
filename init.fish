bfa30f24a55687bc2bf69470a1721b5be6ac0a5f
Viet Anh Duong <vietanhs0817@gmail.com>
2025-04-23 13:13:11 +0700

    Update fish
    Signed-off-by: Viet Anh Duong <vietanhs0817@gmail.com>

--- init.fish
+++ init.fish
@@ -146,12 +146,13 @@ end
 # customize fish prompt
 #
 function fish_prompt --description 'Write out the prompt'
+    # Store status immediately at the start of the function
+    set -l last_status $status
     set -l last_pipestatus $pipestatus
-    set -lx __fish_last_status $status # Export for __fish_print_pipestatus.
+
+    # Don't modify $status until necessary
     set -l normal (set_color normal)
-    set -q fish_color_status
     set -l vcs_color (set_color brpurple)
-    or set -g fish_color_status red
 
     # Color the prompt differently when we're root
     set -l color_cwd $fish_color_cwd
@@ -163,19 +164,18 @@ function fish_prompt --description 'Write out the prompt'
         set suffix '#'
     end
 
-    # Write pipestatus
-    # If the status was carried over (if no command is issued or if `set` leaves the status untouched), don't bold it.
-    set -l bold_flag --bold
-    set -q __fish_prompt_status_generation; or set -g __fish_prompt_status_generation $status_generation
-    if test $__fish_prompt_status_generation = $status_generation
-        set bold_flag
-    end
-    set __fish_prompt_status_generation $status_generation
-    set -l status_color (set_color $fish_color_status)
-    set -l statusb_color (set_color $bold_flag $fish_color_status)
+    # Handle status colors
+    set -l status_color (set_color $fish_color_status; or set_color red)
+    set -l statusb_color (set_color --bold $fish_color_status; or set_color --bold red)
+
+    # Only set __fish_last_status right before it's needed
     set -l prompt_status (__fish_print_pipestatus "[" "]" "|" "$status_color" "$statusb_color" $last_pipestatus)
 
+    # Final output
     echo -n -s (prompt_login)' ' (set_color $color_cwd) (prompt_pwd) $vcs_color(fish_vcs_prompt) $normal " "$prompt_status " " $suffix " "
+
+    # Return the original status
+    return $last_status
 end
 
 
