c1cf86f7dab145a9296e062fc04d8db6e390ec5f
Viet Anh Duong <vietanhs0817@gmail.com>
2025-09-29 14:00:53 +0700

    Update config
    Signed-off-by: Viet Anh Duong <vietanhs0817@gmail.com>

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -274,13 +274,18 @@ vim.opt.rtp:prepend(lazypath)
 -- then, setup!
 require("lazy").setup {
     {
-        "blazkowolf/gruber-darker.nvim",
+        --"blazkowolf/gruber-darker.nvim",
+        "navarasu/onedark.nvim",
         lazy = false,    -- load at start
         priority = 1000, -- load first
         config = function()
             vim.o.termguicolors = true
             vim.o.background = "dark"
-            vim.cmd [[colorscheme gruber-darker]]
+            require('onedark').setup {
+                style = 'warmer'
+            }
+            -- Enable theme
+            require('onedark').load()
 
             --local bools = vim.api.nvim_get_hl(0, { name = "Boolean" })
             --vim.api.nvim_set_hl(0, "Comment", bools)
