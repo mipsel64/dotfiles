47584746b61b38d7da5f516c163a76607e222983
Viet Anh Duong <vietanhs0817@gmail.com>
2025-10-08 09:24:19 +0700

    Update config
    Signed-off-by: Viet Anh Duong <vietanhs0817@gmail.com>

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -276,7 +276,6 @@ vim.opt.rtp:prepend(lazypath)
 -- then, setup!
 require("lazy").setup {
     {
-        --"blazkowolf/gruber-darker.nvim",
         "chriskempson/base16-vim",
         lazy = false,    -- load at start
         priority = 1000, -- load first
@@ -284,16 +283,17 @@ require("lazy").setup {
             vim.o.termguicolors = true
             vim.o.background = "dark"
             vim.cmd([[colorscheme base16-default-dark]])
+            vim.api.nvim_set_hl(0, "Normal", { bg = "#2b2b2b" })
 
-            --local bools = vim.api.nvim_get_hl(0, { name = "Boolean" })
-            --vim.api.nvim_set_hl(0, "Comment", bools)
-            --local marked = vim.api.nvim_get_hl(0, { name = "PMenu" })
+            local bools = vim.api.nvim_get_hl(0, { name = "Boolean" })
+            vim.api.nvim_set_hl(0, "Comment", bools)
+            local marked = vim.api.nvim_get_hl(0, { name = "PMenu" })
 
-            --vim.api.nvim_set_hl(
-            --    0,
-            --    "LspSignatureActiveParameter",
-            --    { fg = marked.fg, bg = marked.bg, ctermfg = marked.ctermfg, ctermbg = marked.ctermbg, bold = true }
-            --)
+            vim.api.nvim_set_hl(
+                0,
+                "LspSignatureActiveParameter",
+                { fg = marked.fg, bg = marked.bg, ctermfg = marked.ctermfg, ctermbg = marked.ctermbg, bold = true }
+            )
         end,
     },
     {
