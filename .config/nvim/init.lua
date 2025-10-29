6c9ed49a6de74daefa657ffed1e8067125ccfa9f
Viet Anh Duong <vietanhs0817@gmail.com>
2025-10-29 12:07:41 +0700

    Update config
    Signed-off-by: Viet Anh Duong <vietanhs0817@gmail.com>

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -276,24 +276,14 @@ vim.opt.rtp:prepend(lazypath)
 -- then, setup!
 require("lazy").setup {
     {
-        "chriskempson/base16-vim",
+        "metalelf0/jellybeans-nvim",
         lazy = false,    -- load at start
         priority = 1000, -- load first
+        dependencies = 'rktjmp/lush.nvim',
         config = function()
             vim.o.termguicolors = true
             vim.o.background = "dark"
-            vim.cmd([[colorscheme base16-default-dark]])
-            vim.api.nvim_set_hl(0, "Normal", { bg = "#2b2b2b" })
-
-            local bools = vim.api.nvim_get_hl(0, { name = "Boolean" })
-            vim.api.nvim_set_hl(0, "Comment", bools)
-            local marked = vim.api.nvim_get_hl(0, { name = "PMenu" })
-
-            vim.api.nvim_set_hl(
-                0,
-                "LspSignatureActiveParameter",
-                { fg = marked.fg, bg = marked.bg, ctermfg = marked.ctermfg, ctermbg = marked.ctermbg, bold = true }
-            )
+            vim.cmd([[colorscheme jellybeans-nvim]])
         end,
     },
     {
