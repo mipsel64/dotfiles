3d0c22c3c31cc67964ba729d5513deb3a1abdbab
Viet Anh Duong <vietanhs0817@gmail.com>
2026-04-12 23:37:35 +0700

    feat: Update neovim
    Signed-off-by: Viet Anh Duong <vietanhs0817@gmail.com>

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -623,7 +623,7 @@ require("lazy").setup {
         config = function()
             require('lualine').setup({
                 options = {
-                    theme = 'onehalfdark',
+                    theme = 'auto',
                     section_separators = { left = '', right = '' },
                     component_separators = { left = '', right = '' },
                 },
