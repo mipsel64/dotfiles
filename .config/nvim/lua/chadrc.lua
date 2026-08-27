f5d943b9cb4ce7e890275fc4efc048f1331e73db
Viet Anh Duong <vietanhs0817@gmail.com>
2026-08-27 15:17:41 +0700

    Update config
    Signed-off-by: Viet Anh Duong <vietanhs0817@gmail.com>

--- .config/nvim/lua/chadrc.lua
+++ .config/nvim/lua/chadrc.lua
@@ -1,13 +1,38 @@
 local M = {}
 
+local sep_l = "  "
+local sep_r = "  "
+
+-- flat statusline: white text, transparent bg, arrows only as shape
+local stl_hl = {}
+for _, g in ipairs {
+  "StatusLine", "St_gitIcons", "St_Lsp", "St_LspMsg", "St_EmptySpace", "ST_EmptySpace",
+  "St_file", "St_file_sep", "St_cwd_icon", "St_cwd_text", "St_cwd_sep",
+  "St_pos_sep", "St_pos_icon", "St_pos_text",
+  "St_lspError", "St_lspWarning", "St_LspHints", "St_LspInfo",
+} do
+  stl_hl[g] = { fg = "white", bg = "NONE", bold = false }
+end
+for m, col in pairs {
+  Normal = "nord_blue",
+  Visual = "cyan",
+  Insert = "dark_purple",
+  Terminal = "green",
+  NTerminal = "yellow",
+  Replace = "orange",
+  Confirm = "teal",
+  Command = "green",
+  Select = "blue",
+} do
+  stl_hl["St_" .. m .. "Mode"] = { fg = col, bg = "NONE", bold = true }
+  stl_hl["St_" .. m .. "ModeSep"] = { fg = col, bg = "NONE" }
+end
+
 M.base46 = {
   theme = "myforest",
   transparency = true,
-  hl_override = {
-    -- TelescopeMatching = { fg = "cyan" },
-    -- TelescopeSelection = { fg = "cyan" },
-    -- TelescopeSelectionCaret = { fg = "cyan" },
-  },
+  hl_override = stl_hl,
+  hl_add = { ST_EmptySpace = { fg = "white", bg = "NONE" } },
 }
 
 M.ui = {
@@ -20,10 +45,20 @@ M.ui = {
 
   statusline = {
     enabled = true,
-    theme = "default",         -- default/vscode/vscode_colored/minimal
-    separator_style = "block", -- default/round/block/arrow
+    theme = "default", -- "minimal" hardcodes block separators, arrow is ignored there
+    separator_style = { left = sep_l, right = sep_r },
     order = { "mode", "file", "git", "%=", "lsp_msg", "%=", "diagnostics", "noice", "lsp", "cwd", "cursor" },
     modules = {
+      -- upstream emits two separators in a row; fine for solid arrows, doubled-up for chevrons
+      mode = function()
+        local utils = require "nvchad.stl.utils"
+        if not utils.is_activewin() then
+          return ""
+        end
+        local m = utils.modes[vim.api.nvim_get_mode().mode]
+        return "%#St_" .. m[2] .. "Mode# " .. m[1] .. "%#St_" .. m[2] .. "ModeSep#" .. sep_r
+      end,
+
       noice = function()
         local reg = vim.fn.reg_recording()
         if reg ~= "" then
@@ -34,7 +69,6 @@ M.ui = {
     },
   },
   telescope = { style = "bordered" },
-
 }
 
 M.lsp = { signature = false }
